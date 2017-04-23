f808a2c75063d2356cda30c73b1f305f507c113a
Sam Adams <sbadams@gmail.com>
2017-04-23 21:34:37 +0100

    Change page name to symbol for readability

--- src/test/scala/example/SearchSpec.scala
+++ src/test/scala/example/SearchSpec.scala
@@ -7,10 +7,10 @@ class SearchSpec extends GoogleSpec {
   import pages._
 
   "After searching the results page" should "have the correct title" in {
-    go to home and { home =>
-      home search "Cheese!"
-    } lastly { results =>
-      results checkTitle
+    go to home and { > =>
+      > search "Cheese!"
+    } lastly { > =>
+      > checkTitle
     }
   }
 }
